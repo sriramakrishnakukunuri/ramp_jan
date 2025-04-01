b6e01ce23797ad2a2ac376f56bbc4a0e3c4ea5e5
saikumar <saikumar.patnaikuni@kellton.com>
2025-04-01 20:18:29 +0530

    changes

--- src/app/TIHCL/loan-application-form/loan-application-form.component.ts
+++ src/app/TIHCL/loan-application-form/loan-application-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+declare var bootstrap: any;
 @Component({
   selector: 'app-loan-application-form',
   templateUrl: './loan-application-form.component.html',
@@ -7,13 +9,41 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class LoanApplicationFormComponent implements OnInit {
 
-  constructor(private toastrService: ToastrService,) { }
+  @ViewChild('exampleModal') exampleModal!: ElementRef;
+  constructor(private toastrService: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   submitForm(){
     this.toastrService.success('Application Form submitted successfully!');
+  }
+
+  checkValidate(value: string): void {
+    if (value === 'yes') {
+      const modal = bootstrap.Modal.getInstance(this.exampleModal.nativeElement);
+      modal.hide();      
+      this.router.navigate(['/login']);
+    } else {
+      const modal = bootstrap.Modal.getInstance(this.exampleModal.nativeElement);
+      modal.hide();
+    }
+  }
+
+  validateEnterpriseCategory(event: any): void {
+    const selectedValue = event.target.value;
+    if (selectedValue === 'Medium' || selectedValue === 'Large') {
+      const modal = new bootstrap.Modal(this.exampleModal.nativeElement);
+      modal.show();      
+    }
+  }
+
+  validateNatureOfActivity(event: any): void {
+    const selectedValue = event.target.value;
+    if (selectedValue === 'Services' || selectedValue === 'Trading') {
+      const modal = new bootstrap.Modal(this.exampleModal.nativeElement);
+      modal.show();
+    }
   }
 
 }
