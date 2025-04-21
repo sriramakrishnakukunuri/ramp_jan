59e82ea4f396d65eb1a159ae17ca47053224fd03
saikumar <saikumar.patnaikuni@kellton.com>
2025-04-21 12:03:34 +0530

    changes

--- src/app/TIHCL/loan-application-form/loan-application-form.component.ts
+++ src/app/TIHCL/loan-application-form/loan-application-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 declare var bootstrap: any;
+declare var $: any;
 @Component({
   selector: 'app-loan-application-form',
   templateUrl: './loan-application-form.component.html',
@@ -13,10 +14,53 @@ export class LoanApplicationFormComponent implements OnInit {
   constructor(private toastrService: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+    var currentStep = 1;
+    var updateProgressBar = function() {
+      var progressPercentage = ((currentStep - 1) / 2) * 100;
+      $(".progress-bar").css("width", progressPercentage + "%");
+    }
+
+    $(document).ready(function() {
+      $('#multi-step-form').find('.step').slice(1).hide();
+  
+      $(".next-step").click(function() {
+      if (currentStep < 3) {
+          $(".step-" + currentStep).addClass("animate__animated animate__fadeIn");
+          currentStep++;
+          setTimeout(function() {
+          $(".step").removeClass("animate__animated animate__fadeIn").hide();
+          $(".step-" + currentStep).show().addClass("animate__animated animate__fadeIn");
+          updateProgressBar();
+          }, 500);
+      }
+      });
+
+      $(".prev-step").click(function() {
+      if (currentStep > 1) {
+          $(".step-" + currentStep).addClass("animate__animated animate__fadeIn");
+          currentStep--;
+          setTimeout(function() {
+          $(".step").removeClass("animate__animated animate__fadeIn").hide();
+          $(".step-" + currentStep).show().addClass("animate__animated animate__fadeIn");
+          updateProgressBar();
+          }, 500);
+      }
+      });
+
+      updateProgressBar = function() {
+      var progressPercentage = ((currentStep - 1) / 2) * 100;
+      $(".progress-bar").css("width", progressPercentage + "%");
+      }
+  });
+ 
   }
 
   submitForm(){
     this.toastrService.success('Application Form submitted successfully!');
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
   }
 
   checkValidate(value: string): void {
