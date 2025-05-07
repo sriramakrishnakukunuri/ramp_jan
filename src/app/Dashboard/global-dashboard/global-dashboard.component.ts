83f69f4491f371027ed6d000c1018faf473fed0f
saikumar <saikumar.patnaikuni@kellton.com>
2025-05-08 01:14:21 +0530

    changes

--- src/app/Dashboard/global-dashboard/global-dashboard.component.ts
+++ src/app/Dashboard/global-dashboard/global-dashboard.component.ts
@@ -16,8 +16,11 @@ export class GlobalDashboardComponent implements OnInit {
   ngOnInit(): void {
     console.log('https://lookerstudio.google.com/embed/reporting/fcab1a0e-f906-4487-85fa-8097553cbc4b/page/p_iqwy179urd')
     this.url = this.sanitizer
-    .bypassSecurityTrustResourceUrl('https://lookerstudio.google.com/embed/reporting/fcab1a0e-f906-4487-85fa-8097553cbc4b/page/p_iqwy179urd');
+    .bypassSecurityTrustResourceUrl('https://lookerstudio.google.com/embed/reporting/fcab1a0e-f906-4487-85fa-8097553cbc4b/page/p_iqwy179urd?cache=' + this.cacheBuster);
   }
+
+  // this.url = this.sanitizer
+  //   .bypassSecurityTrustResourceUrl('https://lookerstudio.google.com/embed/reporting/fcab1a0e-f906-4487-85fa-8097553cbc4b/pages/p_iqwy179urd?cache=' + this.cacheBuster);
   
   //https://lookerstudio.google.com/embed/reporting/e52e2113-3d3a-4941-a7f3-57d9971064c4/page/rwZEF
   //https://lookerstudio.google.com/reporting/fcab1a0e-f906-4487-85fa-8097553cbc4b
