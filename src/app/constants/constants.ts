6e51a2f3b069c88912b45d9b53f352ce3e9037ba
venkataramgithub <venkataramulu9100@gmail.com>
2025-05-24 14:30:05 +0530

    ramulu latest update

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -125,5 +125,12 @@ export const APIS = {
         // getProgramMonitoringById:API_BASE_URL +`/program/monitoring/`,
         saveProgramMonitoring:API_BASE_URL +`/program/feedback/save`,
         updateProgramMonitoring:API_BASE_URL +`/program/feedback/update/`,
-    }
+    },
+    collageCreation:{
+        GET_ALL_IMAGES: API_BASE_URL+`/program/file/paths/`,
+        GET_COLLAGE_IMAGES: API_BASE_URL+`/program/file/paths/status?fileType=COLLAGE`,
+        UPLOAD_COLLAGE: API_BASE_URL+`/program/collage/images`,
+        GET_AGENCIES: API_BASE_URL+`/agencies`,
+        GET_PROGRAMS: API_BASE_URL+`/agency/programs/dropdown`,
+    },
 }
