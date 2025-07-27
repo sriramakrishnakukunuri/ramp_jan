03cdff0a0cb65a5bfb0478a37d54affad4e95299
upendranathreddy021 <upendrareddy021@gmail.com>
2025-07-27 14:15:16 +0530

    9 dots menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,23 @@ export class AppComponent {
     get isAdmin() {
         return this.user?.userRole === Role.Admin;
     }
+// This is to handle the submenu toggle functionality --added by upendranath reddy || 27th july 2025
+    menuOpen = false;
+openSubMenus: {[key: string]: boolean} = {};
+
+toggleSubMenu(menu: string, event: Event) {
+  event.preventDefault();
+  event.stopPropagation();
+  // If the submenu is already open, close it. Otherwise, close all and open the clicked one.
+  if (this.openSubMenus[menu]) {
+    this.openSubMenus[menu] = false;
+  } else {
+    Object.keys(this.openSubMenus).forEach(key => {
+      this.openSubMenus[key] = false;
+    });
+    this.openSubMenus[menu] = true;
+  }
+}
 
     logout() {
         this.authenticationService.logout();
