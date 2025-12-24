1e9e074d05f5d18f344484861df6e53f94850ecc
Ramakrishna <sriramakrishna.kukunuri@kellton.com>
2025-12-24 15:18:08 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // apiUrl:'https://tihcl.com/tihcl/api',
-    apiUrl:`http://52.4.130.165:8081/tihcl/api`
+    apiUrl:'https://tihcl.com/tihcl/api',
+    // apiUrl:`http://52.4.130.165:8081/tihcl/api`
 };
