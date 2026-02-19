93f2dc2cfa3d2a698ad295275ec57598506e730c
Ramakrishna <sriramakrishna.kukunuri@kellton.com>
2026-02-19 12:14:31 +0530

    changes

--- src/app/Dashboard/global-dashboard/global-dashboard.component.ts
+++ src/app/Dashboard/global-dashboard/global-dashboard.component.ts
@@ -28,9 +28,13 @@ export class GlobalDashboardComponent implements OnInit {
 
 
   setDashboardUrl(): void {
+   
+    console.log('EXECUTIVE_MANAGER','https://lookerstudio.google.com/embed/u/0/reporting/52880037-9096-4a97-9068-aeefbb359054/page/CQfUF?params=%7B%22id%22:%22'+this.loginsessionDetails.userId+'%22%7D');
     if(this.loginsessionDetails && this.loginsessionDetails.userRole=='EXECUTIVE_MANAGER') {
+       
       this.url = this.sanitizer.bypassSecurityTrustResourceUrl(
-        'https://lookerstudio.google.com/embed/reporting/52880037-9096-4a97-9068-aeefbb359054/page/PAGE_7_ID?params=%7B%22id%22%3A%22'+this.loginsessionDetails.userId+'5%22%7D'
+        'https://lookerstudio.google.com/embed/u/0/reporting/52880037-9096-4a97-9068-aeefbb359054/page/CQfUF?params=%7B%22id%22:%22'+this.loginsessionDetails.userId+'%22%7D'
+        // 'https://lookerstudio.google.com/embed/reporting/52880037-9096-4a97-9068-aeefbb359054/page/PAGE_7_ID?params=%7B%22id%22%3A%22'+this.loginsessionDetails.userId+'%22%7D'
       );
     }
     else{
