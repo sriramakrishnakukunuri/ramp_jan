c6b119104d9cbbec03a4c85bb7e83ccfe8f95945
Saic98779 <saichaitanya550@gmail.com>
2026-03-30 00:04:01 +0530

    prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://10.3.38.49:8081/workflow'
+    apiUrl: 'http://10.3.38.49:8081/workflow',
+    fileUrl: 'http://10.3.38.49/workflowfiles'
 };
