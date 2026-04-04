a75482ddec06ff33d9179917728748744f9c0dc1
Saic98779 <saichaitanya550@gmail.com>
2026-04-04 11:27:00 +0530

    changes related to

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://10.3.38.49:8081/workflow',
-    fileUrl: 'http://10.3.38.49/workflowfiles'
+    apiUrl: 'http://ramp.telangana.gov.in/workflow',
+    fileUrl: 'http://ramp.telangana.gov.in/workflowfiles'
 };
